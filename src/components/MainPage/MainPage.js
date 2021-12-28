07e534a9f9079f909988aea9e649430082a0daa4
manuel <manuel@arenacast.io>
2021-12-28 16:05:21 +0900

    adding axios response

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 function MainPage() {
+
+    const [events, setEvents] = React.useState([]);
+
+
+    React.useEffect(async () => {
+            const getEvents = async () => {
+                await axios.get('https://mobile-app-interview.awair.is/events')
+                .then((response) => {
+                    console.log('response:', response);
+                    setEvents(response);
+                    console.log('Events: ', events);
+                })
+                .catch((error) => {
+                    console.log('error: ', error);
+                });
+            }
+            if (events.length === 0) {
+                getEvents();
+            }
+      }, [events]);
+      console.log('events out: ', events);
+
     return (
         <div>
-            Hello there
+                <div style={{ display: 'flex'}}>
+                    <div>{events.length > 0 ? events.data.events[0].title : 'fuck you'}</div>
+                </div>
         </div>
     );
 }
